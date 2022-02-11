e2f13b32f18c98da9c24b10abc90a4b0a388b968
Jonathan Armoza <jarmoza@gmail.com>
2022-02-11 16:10:36 -0500

    Merge of annotation mockup changes

--- pages/annotation.vue
+++ pages/annotation.vue
@@ -1,8 +1,20 @@
 <template>
 
     <b-container fluid>
+
 		<!-- Navigation bar -->
-		<tool-navbar :navItemsState="navItemsState"></tool-navbar>
+		<tool-navbar :navItemsState="navItemsState" :pageName="fullName"></tool-navbar>
+
+        <b-row>
+            <b-col cols="4"></b-col>
+            <b-col cols="4">
+                <img class="vertical-center-mockup"
+                     :src="require(`~/assets/${mockupImage.filename}`)"
+                     :width="mockupImage.width"
+                     :height="mockupImage.height"/>
+            </b-col>
+            <b-col cols="4"></b-col>
+        </b-row>        
 
     </b-container>
 
@@ -16,6 +28,16 @@
         data() {
 
             return {
+
+                // Full text name of this page
+				fullName: this.$store.getters.pageNames.categorization.fullName, 
+
+                // Image for temporary annotation page mockup
+                mockupImage: {
+                    filename: "annotation_mockup.jpg",
+                    height: 956,
+                    width: 1045
+                },
 
                 // Initial status of the navbar items for other pages
                 navItemsState: [
@@ -40,5 +62,14 @@
 
 </script>
 
-<style>
+<style scoped>
+
+.vertical-center-mockup {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    /*-ms-transform: translateY(50%);*/
+    /*transform: translateY(50%);*/
+}
+
 </style>
