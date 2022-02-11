bd072cd36578b5aa81198aab829d08422808d6b0
Jonathan Armoza <jarmoza@gmail.com>
2022-02-11 15:24:05 -0500

    Debug configuration for Nuxt and VS Code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,54 +1,61 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'annotation_tool',
-    htmlAttrs: {
-      lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+	
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+		title: "annotation_tool",
+		htmlAttrs: {
+			lang: "en"
+		},
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: "" },
+			{ name: "format-detection", content: "telephone=no" }
+		],
+		link: [
+			{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
+		]
+	},
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-	'@/plugins/bootstrap-vue',
-  ],
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [
+	],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [
+		"@/plugins/bootstrap-vue",
+	],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+	],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/bootstrap
+		"bootstrap-vue/nuxt",
+		// https://go.nuxtjs.dev/axios
+		"@nuxtjs/axios",
+	],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {
+		// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+		baseURL: "/",
+	},
 
-  devtool: 'source-map',
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {
+		extend(p_config, p_ctx) {
+			if ( p_ctx.isDev ) {
+				p_config.devtool = p_ctx.isClient ? "source-map" : "inline-source-map"
+			}
+		}
+	},
+
+	devtool: "source-map",
 }
