3185390027e9da12d14a727086f5c4663ee7c080
jarmoza <jarmoza@gmail.com>
2022-02-23 10:49:23 -0500

    Adding in missing config for main.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,9 @@ export default {
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
 	css: [
-	],
+
+		"@/assets/css/main.css"
+	]
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
