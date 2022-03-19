94e54c0a3cac31acd2523566c75f93adb286d4ba
surchs <sebastian.urchs@gmail.com>
2022-03-19 00:27:43 -0400

    [MAINT] added prettier for code formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,22 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-      // "airbnb", // TODO: activate this styleset
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    // "airbnb", // TODO: activate this styleset
+    "eslint:recommended",
+    "plugin:vue/essential",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["vue"],
+  rules: {},
+  globals: {
+    $nuxt: true,
+  },
+};
