1233ca0c3f3d271cba1427712311f9b2298f1934
surchs <sebastian.urchs@gmail.com>
2022-03-19 00:11:30 -0400

    [MAINT] added and configured ESLint with Vue plugin
    Using all default configuration. We will revisit the rules.
    
    Resolves #74

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+      // "airbnb", // TODO: activate this styleset
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
