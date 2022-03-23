aa60055e8a0604bfc6f78d021147c6eb0d692db8
surchs <sebastian.urchs@gmail.com>
2022-03-23 18:04:25 -0400

    added vue-select plugin
    - vue-bootstrap does not have a good option for drop-down selection with string autocompletion so we're using https://vue-select.org/ here instead

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-	
+
 	// Global page headers: https://go.nuxtjs.dev/config-head
 	head: {
 
@@ -26,7 +26,8 @@ export default {
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
-		"@/plugins/bootstrap-vue"
+		"@/plugins/bootstrap-vue",
+    "@/plugins/vue-select"
 	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/vue-select.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import vSelect from 'vue-select'
+
+Vue.component('v-select', vSelect)
+
+import 'vue-select/dist/vue-select.css';
