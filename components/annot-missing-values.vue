c72f5987ec8bcdb25386d5573b8665b73ba9fc6a
surchs <sebastian.urchs@gmail.com>
2022-05-04 10:20:19 -0400

    missing Value Component viewer
    - component reactively shows global store
    - filters by relevantColumns
    - has event emitter to "remove value" and have this be stored in store

--- components/annot-missing-values.vue
+++ components/annot-missing-values.vue
@@ -28,7 +28,7 @@
 <script>
 
     // Allows for reference to store data by creating simple, implicit getters
-    import { mapGetters } from "vuex";
+    import { mapGetters, mapState } from "vuex";
 
     export default {
 
@@ -38,8 +38,6 @@
 
             relevantColumns: { type: Array, required: true }
         },
-
-        inject: ["missingColumnValues"],
 
         data() {
 
@@ -65,31 +63,40 @@
 
             ...mapGetters([
 
-                "isMissingValue",
                 "valueDescription"
             ]),
 
+            ...mapState([
+
+                "missingColumnValues"
+            ]),
+
             tableItems() {
-                const tmp = [
-                    {
-                        column: 'sex',
-                        description: 'my description',
-                        value: 'my value'
+                let missingValueArray = []
+
+                for ( let column of this.relevantColumns ) {
+                    if ( Object.keys(this.missingColumnValues).includes(column) ) {
+                        for ( let missing_value of this.missingColumnValues[column] ) {
+                            const description = this.valueDescription(column, missing_value);
+                            missingValueArray.push(
+                                {
+                                    column: column,
+                                    description: description === null ? "" : description,
+                                    value: missing_value
+                                }
+                            )
+
+                        }
                     }
-                ]
-                return tmp
+                }
+                return missingValueArray
             }
         },
 
         methods: {
 
             removeColumn(p_tableItem) {
-
-                // 1. Remove the item from table data
-                this.tableItems = this.tableItems.filter(item =>
-                    item.column === p_tableItem.column && item.value !== p_tableItem.value );
-
-                // 2. Remove this value from the column's missing value list in the store
+                // Remove this value from the column's missing value list in the store
                 this.$emit('remove:missingValue', p_tableItem);
             }
         }
