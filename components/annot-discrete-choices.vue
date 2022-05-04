1cbd60be04286205da1519667a40020fde075946
surchs <sebastian.urchs@gmail.com>
2022-05-04 11:57:39 -0400

    missing value UI element added
    - discrete values component now has an extra UI element (red button) for each unique value that can label this value as "missing"
    - so far it doesn't do anything beyond being visible

--- components/annot-discrete-choices.vue
+++ components/annot-discrete-choices.vue
@@ -154,7 +154,7 @@
 
                     // There has been at least one column removed from this component's category,
                     // possibly via the annot-columns component 'remove' action
-                    for (const columnName of removedColumns) {
+                    for ( const columnName of removedColumns ) {
 
                         // We cannot just remove the key from the object with a normal
                         // JS delete operator because then Vue wouldn't be aware of it.
