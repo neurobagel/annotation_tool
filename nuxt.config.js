f305633d4623d894af693f1c6124081d1472e627
Jonathan Armoza <jarmoza@gmail.com>
2022-05-30 11:55:25 -0400

    Missing space in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
 		"@/plugins/bootstrap-vue",
-    "@/plugins/vue-select"
+        "@/plugins/vue-select"
 	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
