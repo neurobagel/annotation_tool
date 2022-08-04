cbcfa4158034cac5c5ea077cf9f883453b4e133a
Jonathan Armoza <jarmoza@gmail.com>
2022-08-04 14:05:20 -0400

    Cypress config: clear downloads folder

--- cypress.config.js
+++ cypress.config.js
@@ -1,34 +1,43 @@
 const { defineConfig } = require("cypress");
 
-const { isFileExist, findFiles } = require('cy-verify-downloads');
+const { isFileExist, findFiles } = require("cy-verify-downloads");
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
 
-    component: {
-      devServer: {
-        bundler: "webpack",
-        framework: "nuxt"
-      }
+    e2e: {
+
+        baseUrl: "http://localhost:3000",
+
+        component: {
+
+            devServer: {
+
+                bundler: "webpack",
+                framework: "nuxt"
+            }
+        },
+
+        // Downloads location and behavior
+        downloadsFolder: "cypress/downloads",
+        trashAssetsBeforeRuns: true,
+
+        setupNodeEvents(on, config) {
+
+            // Implement node event listeners here
+
+            // cy-verify-downloads plugin
+            on("task", { isFileExist, findFiles });
+        }
     },
 
-    downloadsFolder: "cypress/downloads",
+    component: {
 
-    setupNodeEvents(on, config) {
-      // Implement node event listeners here
+        devServer: {
 
-      // cy-verify-downloads plugin
-      on('task', { isFileExist, findFiles });
+            bundler: "webpack",
+            framework: "nuxt"
+        }
     }
-  },
 
-  component: {
-    devServer: {
-      framework: "nuxt",
-      bundler: "webpack"
-    }
-  },
 
-  
 });
