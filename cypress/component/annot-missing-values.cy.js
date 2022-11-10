7440fe3f72ff2c1615b7d208d23b25d5dcb3e4e2
Sebastian Urchs <surchs@users.noreply.github.com>
2022-11-10 00:02:59 -0500

    Refactor and test missing value component (#222)
    * Refactored input data
    
    - receives active category via prop (from parent)
    - receives missing values via getter
    - receives descriptions for values via getter
    
    * Component test for store mutation
    
    - mocked store mutation
    - listen to mocked mutation being called via spy
    - implement the mutation call in the component
    
    * keep store mocks in store object

---
+++ cypress/component/annot-missing-values.cy.js
@@ -0,0 +1,59 @@
+import annotMissingValues from "~/components/annot-missing-values";
+
+
+// Mocked Store getters
+const store = {
+    getters: {
+        valueDescription: () => (column, missingValue) => missingValue + " from " + column,
+        missingValues: () => (category) => {
+            return {
+                "column1": ["val1", "val2"],
+                "column2": ["val3"]
+            };
+        }
+    }
+};
+
+const props = {
+    activeCategory: "category1"
+};
+
+describe("missing values", () => {
+
+        it('displays unique values and description', () => {
+                cy.mount(annotMissingValues, {
+                        propsData: props,
+                        computed: store.getters
+                    }
+                );
+                cy.get('.missing-values-card-body').contains('val1 from column1');
+            }
+        );
+
+        it('handles lack of description gracefully', () => {
+                cy.mount(annotMissingValues, {
+                        propsData: props,
+                        computed: Object.assign(store.getters, {valueDescription: () => (col, mis) => null})
+                    }
+                );
+            }
+        );
+
+        it("can be declared 'not missing' by clicking the 'Not Missing' button", () => {
+                const mockStore = {commit: () => {}};
+                cy.spy(mockStore, 'commit').as('commitSpy');
+
+                cy.mount(annotMissingValues, {
+                        propsData: props,
+                        computed: store.getters,
+                        mocks: {
+                            $store: mockStore
+                        }
+                    }
+                );
+                cy.get("[data-cy='not-missing-button-column1-val1']").click();
+                cy.get("@commitSpy").should('have.been.calledWith', "declareNotMissing", {column: "column1", value: "val1"});
+            }
+        );
+    }
+);
