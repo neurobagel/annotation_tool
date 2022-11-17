2d03ed06b49ca55b3cd2325fbe7fb9fb52911ac5
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-11-17 16:22:52 -0500

    Created generic ann-categorical vue component
    - refactored the component to get the data directly from the store
    - has missing value button
    
    Co-authored-by: Sebastian Urchs <surchs@users.noreply.github.com>

---
+++ components/annot-categorical.vue
@@ -0,0 +1,123 @@
+<template>
+
+    <div>
+
+        <b-card
+            no-body
+            class="annotation-card">
+
+            <b-card-header>{{ uiText.instructions }}</b-card-header>
+
+            <b-card-body>
+
+                <b-table
+                    striped
+                    :fields="exampleFields"
+                    data-cy="categoricalTable"
+                    :items="displayTable">
+                    <template #cell(select_an_appropriate_mapping)="row">
+
+                        <!-- Bootstrap-Vue doesn't have a great option here so I am using https://vue-select.org/ -->
+                        <!-- NOTE: We use the $event statement to add the row data to the payload of the @input
+                            event without replacing the original event payload -->
+
+                        <v-select
+                            :data-cy="'categoricalSelector' + '_' + row.index"
+                            :value="getSelectedOption(row.index)"
+                            @input="selectAnOption($event, row.item['columnName'], row.item['rawValue'])"
+                            :options="getOptions()" />
+                    </template>
+                    <template #cell(missingValue)="row">
+                        <b-button
+                            :data-cy="'missingValueButton_' + row.index"
+                            variant="danger"
+                            @click="designateAsMissing(row.item['columnName'], row.item['rawValue'])">
+                            {{ uiText.missingValueButton }}
+                        </b-button>
+                    </template>
+
+                </b-table>
+
+                <b-row />
+
+            </b-card-body>
+
+        </b-card>
+
+    </div>
+
+</template>
+
+<script>
+
+    // Allows for reference to store data by creating simple, implicit getters
+    import { mapGetters, mapMutations } from "vuex";
+
+    export default {
+
+        name: "AnnotCategorical",
+
+        props: {
+            activeCategory: { type: String, required: true }
+        },
+
+        data() {
+
+            return {
+
+                exampleFields: [
+
+                    "columnName",
+                    "rawValue",
+                    "description",
+                    "select_an_appropriate_mapping",
+                    "missingValue"
+                ],
+
+                // Text for UI elements
+                uiText: {
+                    instructions: "Annotate each unique value",
+                    saveButton: "Save Annotation",
+                    missingValueButton: "Mark as missing"
+                },
+
+                valueMapping: {}
+            };
+
+        },
+
+        computed: {
+
+            ...mapGetters([
+
+                "getUniqueValues",
+                "getDescription",
+                "getOptions",
+                "getSelectedOption"
+            ]),
+            displayTable() {
+
+                // Create and return table data for the unique values in the relevant columns that are not missing values
+                const tableData = [];
+                for ( const row of this.getUniqueValues(this.activeCategory) ) {
+
+                    const description = this.getDescription(row.columnName, row.rawValue);
+
+                    tableData.push({
+                        columnName: row.columnName,
+                        rawValue: row.rawValue,
+                        description: description
+                    });
+                }
+                return tableData;
+            }
+        },
+        methods: {
+            ...mapMutations([
+                "selectAnOption",
+                "designateAsMissing"
+            ])
+        }
+    };
+
+</script>
---
+++ cypress/component/annot-categorical.cy.js
@@ -0,0 +1,102 @@
+import annotCategorical from "~/components/annot-categorical.vue";
+
+
+// Mocking the store
+const store = {
+    commit: () => {},
+    getters: {
+        getUniqueValues: () => (activeCategory) => {
+            return [
+                {
+                    columnName: "column1", rawValue: "PD"
+                },
+                {
+                    columnName: "column1", rawValue: "HC"
+                },
+                {
+                    columnName: "column2", rawValue: ""
+                },
+                {
+                    columnName: "column2", rawValue: "oups"
+                }
+            ];
+        },
+        getDescription: () => (column, cellValue) => {
+            return "descr_" + column + "_" + cellValue;
+        },
+        getOptions: () => (category) => {
+            return [
+                "option_0",
+                "option_1",
+                "option_2",
+                "option_3"
+            ];
+        },
+        getSelectedOption: () => (rowIndex) => {
+            return "option_" + rowIndex;
+        }
+    },
+    mutations: {
+        selectAnOption: () => (option, columnName, rawValue) => {
+        },
+        designateAsMissing: () => (columnName, rawValue) => {
+        }
+    }
+};
+
+const props = {
+    activeCategory: "category1"
+};
+
+
+describe("categorical annotation", () => {
+    it("displays unique values and their descriptions", () => {
+        cy.mount(annotCategorical, {
+                computed: store.getters,
+                propsData: props
+            });
+            cy.get("[data-cy='categoricalTable']").contains("PD");
+            cy.get("[data-cy='categoricalTable']").contains("descr_column1_PD");
+        }
+    );
+    it("displays a dropdown, selects an option, informs the store about an created/updated mapping by calling a mutation", () => {
+        cy.spy(store, 'commit').as('commitSpy');
+        cy.mount(annotCategorical, {
+            computed: store.getters,
+            propsData: props,
+            mocks: {
+                $store: store
+            }
+            });
+        cy.get("[data-cy='categoricalTable']").within(() => {
+            cy.get("[data-cy='categoricalSelector_0']").should("be.visible");
+            cy.get("[data-cy='categoricalSelector_1']").should("be.visible");
+            cy.get("[data-cy='categoricalSelector_2']").should("be.visible");
+            cy.get("[data-cy='categoricalSelector_3']").should("be.visible");
+        });
+
+        cy.get("[data-cy='categoricalSelector_0']").click().contains("option_2").click();
+        cy.get("@commitSpy").should("have.been.calledOnceWith", "selectAnOption", "option_2", "column1", "PD");
+    });
+    it("displays the pre-set mapping in the dropdown", () => {
+        cy.mount(annotCategorical, {
+            computed: store.getters,
+            propsData: props
+            });
+        cy.get("[data-cy='categoricalSelector_1']").contains("option_1");
+    });
+    it("displays the missing value button and designates value as missing when clicked", () => {
+        cy.spy(store, 'commit').as('commitSpy');
+        cy.mount(annotCategorical, {
+            computed: store.getters,
+            propsData: props,
+            mocks: {
+                $store: store
+            }
+
+            });
+        cy.get("[data-cy='missingValueButton_0']").should("be.visible");
+        cy.get("[data-cy='missingValueButton_1']").click();
+        cy.get("@commitSpy").should("have.been.calledOnceWith", "designateAsMissing", "column1", "HC");
+    });
+});
