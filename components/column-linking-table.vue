01377f20aa87029aef545dad0ddc73b7c9baf76b
Jonathan Armoza <jarmoza@gmail.com>
2022-11-22 13:21:21 -0500

    PR changes for simplifying component and test

--- components/column-linking-table.vue
+++ components/column-linking-table.vue
@@ -9,7 +9,7 @@
             outlined
             :fields="uiText.tableFields"
             head-variant="dark"
-            :items="columnToCategoryTable"
+            :items="tableRows"
             @row-clicked="applyCategory"
             :tbody-tr-class="styleTableRow" />
 
@@ -19,12 +19,11 @@
 
 <script>
 
-    // Allows for reference to store data by creating simple, implicit getters
-    // Fields listed in mapState below can be found in the store (index.js)
-    import { mapState } from "vuex";
-
     // Allows for reference to store actions (index.js)
     import { mapActions } from "vuex";
+
+    // Allows for reference to store data by creating simple, implicit getters
+    import { mapGetters } from "vuex";
 
     export default {
 
@@ -37,120 +36,58 @@
 
             return {
 
-                columnToCategoryTable: [],
-
                 uiText: {
 
                     tableFields: [
 
                         { key: "column" },
-                        { key: "description" }
+                        { key: "description" },
+                        { key: "category" }
                     ]
                 }
             };
         },
 
-        created() {
-
-            // Create the data structure for the category to column linking table
-            this.setupColumnToCategoryTable();
-        },
-
         computed: {
 
-            ...mapState([
+            ...mapGetters([
 
                 "categoryClasses",
-                "columnToCategoryMap",
-                "dataDictionary",
-                "dataTable"
-            ])
+                "columns",
+                "columnToCategoryMap"
+            ]),
+
+            tableRows() {
+
+                return this.columns.map(column => ({
+
+                    category: this.columnToCategoryMap[column],
+                    column: column.name,
+                    description: column.description
+                }));
+            }
         },
 
         methods: {
 
             ...mapActions([
 
-                "linkColumnWithCategory",
-                "unlinkColumnFromCategory"
+                "alterColumnCategoryRelation"
             ]),
 
             applyCategory(p_row, p_index, p_event) {
 
-                const dataForStore = { column: p_row.column };
+                const payload = {
+
+                    category: this.selectedCategory,
+                    column: p_row.column
+                };
 
                 // 1. Link or unlink the currently-selected category and the clicked column
-                if ( this.selectedCategory !== this.columnToCategoryMap[p_row.column] ) {
-
-                    this.linkColumnWithCategory({...dataForStore, category: this.selectedCategory});
-                } else {
-
-                    this.unlinkColumnFromCategory(dataForStore);
-                }
+                this.alterColumnCategoryRelation(payload);
 
                 // 2. Tell the categorization page a column has been clicked
-                this.$emit("column-name-clicked", dataForStore);
-            },
-
-            setupColumnToCategoryTable() {
-
-                // 0. Check that there is at least a data table and data dictionary in the data store
-                if ( null === this.dataTable.original )
-                    return;
-
-                // 1. Produce an array of dicts
-                this.columnToCategoryTable = [];
-
-                // A. Each dict has a header entry from the data table file
-                const headerFields = [];
-                for ( const headerField in this.dataTable.original[0] ) {
-
-                    // I. Save the header field in a list
-                    headerFields.push(headerField);
-
-                    // II. Save a new dict for this column and description
-                    this.columnToCategoryTable.push({
-
-                        "category": null,
-                        "column": headerField,
-                        "description": ""
-                    });
-                }
-
-                // 2. Add in descriptions if a data dictionary has been supplied
-                if ( null !== this.dataDictionary.original ) {
-
-                    // A. and a corresponding "description" column that is (possibly) sourced from the json file
-                    for ( const column in this.dataDictionary.original ) {
-
-                        // I. Save a lowercase version of the current json key
-                        const columnLowercase = column.toLowerCase();
-
-                        // II. Try to match the json key with one in the tsv file
-                        if ( headerFields.includes(columnLowercase) ) {
-
-                            for ( let index = 0; index < this.columnToCategoryTable.length; index++ ) {
-
-                                // NOTE: Advanced column name matching here between tsv and json? J. Armoza 01/26/22
-                                if ( columnLowercase === this.columnToCategoryTable[index].column.toLowerCase() ) {
-
-                                    // a. Determine the description string for this json file column entry
-                                    let descriptionStr = "";
-                                    for ( const subkey in this.dataDictionary.original[column] ) {
-
-                                        if ( "description" === subkey.toLowerCase() ) {
-                                            descriptionStr = this.dataDictionary.original[column][subkey];
-                                            break;
-                                        }
-                                    }
-
-                                    // b. Save the description from the json file colum entry
-                                    this.columnToCategoryTable[index].description = descriptionStr;
-                                }
-                            }
-                        }
-                    }
-                }
+                this.$emit("column-name-clicked", payload);
             },
 
             styleTableRow(p_row, p_rowType) {
--- cypress/component/column-linking-table.cy.js
+++ cypress/component/column-linking-table.cy.js
@@ -1,3 +1,4 @@
+// import { stubObject } from "cypress/types/lodash";
 import ColumnLinkingTable from "~/components/column-linking-table.vue";
 
 // Documentation for testing Vue events of components
@@ -7,109 +8,69 @@ import ColumnLinkingTable from "~/components/column-linking-table.vue";
 
 const store = {
 
-    state: {
-
-        categories: [
-
-            "Subject ID",
-            "Age",
-            "Sex",
-            "Diagnosis",
-            "Assessment Tool"
-        ],
-
-        categoryClasses: {
-
-            "Subject ID": "category-style-0",
-            "Age": "category-style-1",
-            "Sex": "category-style-2",
-            "Diagnosis": "category-style-3",
-            "Assessment Tool": "category-style-4"
-        },
-
-        columnToCategoryMap: {
-
-            "participant_id": null,
-            "age": null,
-            "sex": null,
-            "group": null,
-            "group_dx": null,
-            "number_comorbid_dx": null,
-            "medload": null,
-            "iq": null,
-            "session": null
-        },
-
-        dataDictionary: {
-
-            original: {
-
-                "age": { "Description": "age of the participant" },
-                "sex": { "Description": "sex of the participant as reported by the participant" },
-                "group": { "Description": "diagnostic status determined by the study clinician at baseline" },
-                "group_dx": { "Description": "specific diagnosis determined by the study clinician at baseline" },
-                "number_comorbid_dx": { "Description": "a number of diagnoses comorbid with UD (e.g., GAD, PTSD)" },
-                "medload": { "Description": "reflects the number of dosage of psychotropic medications taken by participants. Higher numbers correspond to more medications and/or higher medication dosage " },
-                "iq": { "Description": "IQ derived based on the NART assessment." },
-                "session": { "Description": "scanning session" }
-            }
-        },
-
-        dataTable: {
-
-            columns: [
-
-                "participant_id",
-                "age",
-                "sex",
-                "group",
-                "group_dx",
-                "number_comorbid_dx",
-                "medload",
-                "iq",
-                "session"
-            ],
-
-            original: [{
-
-                "participant_id": "",
-                "age": "",
-                "sex": "",
-                "group": "",
-                "group_dx": "",
-                "number_comorbid_dx": "",
-                "medload": "",
-                "iq": "",
-                "session": ""
-            }]
-        },
-
-        toolColorPalette: {
-
-            color1: "category-style-0",
-            color2: "category-style-1",
-            color3: "category-style-2",
-            color4: "category-style-3",
-            color5: "category-style-4",
-            colorDefault: "category-style-default"
-        }
-    },
-
     actions: {
 
-        dispatch: (p_actionName, p_payload) => { }
+        dispatch: (p_actionName, p_payload) => {}
     },
 
-    getters: {}
-};
+    getters: {
 
-store.getters = {
+        categories: () => {
 
-    categories: () => store.state.categories,
-    categoryClasses: () => store.state.categoryClasses,
-    columnToCategoryMap: () => store.state.columnToCategoryMap,
-    dataDictionary: () => store.state.dataDictionary,
-    dataTable: () => store.state.dataTable
+            return [
+
+                "Subject ID",
+                "Age",
+                "Sex",
+                "Diagnosis",
+                "Assessment Tool"
+            ];
+        },
+
+        categoryClasses: () => {
+
+            return {
+
+                "Subject ID": "category-style-0",
+                "Age": "category-style-1",
+                "Sex": "category-style-2",
+                "Diagnosis": "category-style-3",
+                "Assessment Tool": "category-style-4"
+            };
+        },
+
+        columns: () => {
+
+            return [
+
+                { name: "participant_id", description: "" },
+                { name: "age", description: "age of the participant" },
+                { name: "sex", description: "sex of the participant as reported by the participant" },
+                { name: "group", description: "diagnostic status determined by the study clinician at baseline" },
+                { name: "group_dx", description: "specific diagnosis determined by the study clinician at baseline" },
+                { name: "number_comorbid_dx", description: "a number of diagnoses comorbid with UD (e.g., GAD, PTSD)" },
+                { name: "medload", description: "reflects the number of dosage of psychotropic medications taken by participants. Higher numbers correspond to more medications and/or higher medication dosage " },
+                { name: "iq", description: "IQ derived based on the NART assessment." },
+                { name: "session", description: "scanning session" }
+            ];
+        },
+
+        columnToCategoryMap: () => {
+
+            return {
+
+                "participant_id": null,
+                "age": null,
+                "sex": null,
+                "group": null,
+                "group_dx": null,
+                "number_comorbid_dx": null,
+                "medload": null,
+                "iq": null,
+                "session": null
+            };
+        }
+    }
 };
 
 const props = {
@@ -121,7 +82,11 @@ const props = {
 
 describe("Tests basic functionality of the table that links categories with data table columns", () => {
 
-    it("Link one column from the currently selected category", () => {
+    it("Alter link relation (add/remove) between a column and a category", () => {
+
+        // 0. The first category and column
+        const participantIDColumn = store.getters.columns()[0].name;
+        const subjectIDCategory = store.getters.categories()[0];
 
         // 1. Arrange - Set up the spy, mount the component, and bind the spy to it
         const onColumnNameClickedSpy = cy.spy().as("onColumnNameClickedSpy");
@@ -147,82 +112,23 @@ describe("Tests basic functionality of the table that links categories with data
 
         // 2. Action: Link the first column to the current category
         cy.get("[data-cy='column-linking-table-table'] tbody > :nth-child(1) > [aria-colindex='1']")
-            .contains(store.state.dataTable.columns[0])
-            .click()
-            .then(() => {
-
-                // This mimics the 'addColumnCategorization' mutation in the store
-                store.state.columnToCategoryMap[store.state.dataTable.columns[0]] = store.state.categories[0];
-            });
+            .contains(participantIDColumn)
+            .click();
 
         // 3. Assert
 
         // A. Make sure linking action is dispatched to the store
         cy.get("@dispatchSpy").should("have.been.calledWith",
 
-            "linkColumnWithCategory",
-            { column: store.state.dataTable.columns[0], category: store.state.categories[0] }
+            "alterColumnCategoryRelation",
+            { category: subjectIDCategory, column: participantIDColumn  }
         );
 
         // B. Make sure the column linking component emitted the correct column data
         cy.get("@onColumnNameClickedSpy").should("have.been.calledWith", {
 
-            column: store.state.dataTable.columns[0]
-        });
-    });
-
-    it("Unlink one column from the currently selected category", () => {
-
-        // 1. Arrange
-
-        // A. Make it as if the first column and first category have been linked
-        store.state.columnToCategoryMap[store.state.dataTable.columns[0]] = store.state.categories[0];
-
-        // B. Set up the spy, mount the component, and bind the spy to it
-        const onColumnNameClickedSpy = cy.spy().as("onColumnNameClickedSpy");
-        cy.spy(store.actions, "dispatch").as("dispatchSpy");
-        cy.mount(ColumnLinkingTable, {
-
-            mocks: {
-
-                $store: store.actions
-            },
-
-            computed: store.getters,
-
-            listeners: {
-
-                "column-name-clicked": onColumnNameClickedSpy
-            },
-
-            plugins: ["bootstrap-vue"],
-
-            propsData: props
-        });
-
-        // 2. Action: Unlink the first column to the current category
-        cy.get("[data-cy='column-linking-table-table'] tbody > :nth-child(1) > [aria-colindex='1']")
-            .contains(store.state.dataTable.columns[0])
-            .click()
-            .then(() => {
-
-                // This mimics the 'removeColumnCategorization' mutation in the store
-                store.state.columnToCategoryMap[store.state.dataTable.columns[0]] = null;
-            });
-
-        // 3. Assert
-
-        // A. Make sure linking action is dispatched to the store
-        cy.get("@dispatchSpy").should("have.been.calledWith",
-
-            "unlinkColumnFromCategory",
-            { column: store.state.dataTable.columns[0] }
-        );
-
-        // B. Make sure the column linking component emitted the correct column data
-        cy.get("@onColumnNameClickedSpy").should("have.been.calledWith", {
-
-            column: store.state.dataTable.columns[0]
+            category: subjectIDCategory,
+            column: participantIDColumn
         });
     });
 });
