d7952387c71b27012d46f795cee3c2c060af672f
Jonathan Armoza <jarmoza@gmail.com>
2022-11-25 11:28:07 -0500

    New next-page component for tool layout

---
+++ components/next-page.vue
@@ -0,0 +1,83 @@
+<template>
+
+    <!-- Next page button -->
+    <b-row align-h="end">
+        <b-col class="text-right" cols="4">
+
+            <!-- Optional instructions to remind user of criteria to proceed to the next page -->
+            <p class="instructions-text" v-if="!pageAccessible(currentPage)">
+                {{ uiText.instructions[currentPage] }}
+            </p>
+
+            <!-- Button to proceed to the next page -->
+            <!-- Only enabled when next page accessibility criteria have been met -->
+            <b-button
+                class="float-right"
+                data-cy="button-nextpage"
+                :disabled="!pageData[nextPage].accessible"
+                :to="'/' + pageData[nextPage].location"
+                :variant="nextPageButtonColor">
+                {{ uiText.button[currentPage] }}
+            </b-button>
+        </b-col>
+    </b-row>
+
+</template>
+
+<script>
+
+    // Allows for reference to store data by creating simple, implicit getters
+    import { mapGetters } from "vuex";
+
+    // Fields listed in mapState below can be found in the store (index.js)
+    import { mapState } from "vuex";
+
+    export default {
+
+        data() {
+
+            return {
+
+                uiText: {
+
+                    button: {
+
+                        "home": "Next step: Categorize columns",
+                        "categorization": "Next step: Annotate columns",
+                        "annotation": "Next step: Review and download harmonized data"
+                    },
+
+                    instructions: {
+
+                        "home": "",
+                        "categorization": "One column must be categorized as 'Subject ID' and all assessment tools must be grouped to proceed",
+                        "annotation": ""
+                    }
+                }
+            };
+        },
+
+        computed: {
+
+            ...mapGetters([
+
+                "nextPage",
+                "nextPageAccessible",
+                "pageAccessible"
+            ]),
+
+            ...mapState([
+
+                "currentPage",
+                "pageData"
+            ]),
+
+            nextPageButtonColor() {
+
+                // Bootstrap variant color of the button leading to the next page
+                return this.nextPageAccessible ? "success" : "secondary";
+            }
+        }
+    };
+
+</script>
