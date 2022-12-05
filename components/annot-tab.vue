3b8c8d84658f3eefd12c4e17047936bb37bb8dc3
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-12-05 17:26:34 -0500

    implemented new input data
    - prop (pased from parent page) to set own category (activeCategory)
    - store getter to get the correct annotation component for the activeCategory
    - tests to assert over both inputs being understood right

--- components/annot-tab.vue
+++ components/annot-tab.vue
@@ -1,21 +1,21 @@
 <template>
 
-    <div>
+    <div :data-cy="activeCategory">
 
         <!-- Explanation text for this annotation tab -->
-        <annot-explanation :data-cy="'annot-expl-' + title" />
+        <annot-explanation :data-cy="('annot-expl-' + activeCategory)" />
 
         <!-- Lists all the columns linked to the category of this annotation tab -->
-        <annot-columns
-            :active-category="`age`" />
+        <annot-columns :data-cy="('annot-col-' + activeCategory)" :active-category="activeCategory" />
 
         <!-- Lists any values determined to be missing (e.g. potentially invalid) in this tab's columns -->
-        <annot-missing-values />
+        <annot-missing-values :data-cy="('annot-missing-' + activeCategory)" :active-category="activeCategory" />
 
         <!-- Component specializing in the particular kind of annotation for this tab's category -->
         <component
-            :is="annot-explanation"
-            :data-cy="'annot-component-' + title"
+            :is="getAnnotationComponent(activeCategory)"
+            :data-cy="(getAnnotationComponent(activeCategory) + '-' + activeCategory)"
+            :active-category="activeCategory"
             :id-field="idField"
             :title="title" />
 
@@ -30,25 +30,22 @@
 
     export default {
 
+        props: {
+            activeCategory: { type: String, required: true }
+        },
+
         data() {
 
             return {
 
-                details: "",
-                title: "hello",
-
-                // Category of the current annotation tab
-                category: "",
-
-                // String name of the column assigned the 'Subject ID' category in the data table
-                idField: "",
-
-                // Disabled state of the save annotation button
-                saveButtonDisabled: true
+                title: "hello"
             };
         },
 
         computed: {
+            ...mapGetters([
+                "getAnnotationComponent"
+            ])
         }
 
     };
--- cypress/component/annot-tab.cy.js
+++ cypress/component/annot-tab.cy.js
@@ -1,8 +1,34 @@
 import annotTab from "~/components/annot-tab.vue";
 
+// NOTE: because the annot-tab component is a container for other components that we are not
+// mounting here (shallowMount), the UI is always going to be empty.
+// We therefore write assertions on existence of elements, not their visibility.
+// In Cypress, elements with no width / content are considered invisible.
+
+const store = {
+    getters: {
+        getAnnotationComponent: () => (category) => "component1"
+    }
+};
+
+const props = {
+    activeCategory: "category1"
+};
 
 describe("annotTab", () => {
-    it("mounts", () => {
-        cy.mount(annotTab);
+    it("mounts understands the activeCategory prop", () => {
+        cy.mount(annotTab, {
+            propsData: props,
+            computed: store.getters
+        });
+        cy.get("[data-cy='category1']").should('exist');
+
+    });
+    it("gets the correct annotation component name from the store", () => {
+        cy.mount(annotTab, {
+            propsData: props,
+            computed: store.getters
+        });
+        cy.get("[data-cy='category1']").get("[data-cy='component1-category1']").should("exist");
     });
 });
