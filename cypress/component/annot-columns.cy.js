6769460754465287ef8b64b88bb5c5f6a915cb0a
Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>
2022-12-12 11:02:23 -0500

    Refactored annot-columns component
    - Added annot-columns.cy.js file
    - Component now displays mapped columns and their descriptions
    - Component now informs the store when a column is removed/unlinked
    - Modified the component alias to annotColumns
    - Added activeCategory as input to the removeColumn mutation

---
+++ cypress/component/annot-columns.cy.js
@@ -0,0 +1,56 @@
+import annotColumns from "~/components/annot-columns.vue";
+
+
+const store = {
+    commit: () => {},
+    getters: {
+        getMappedColumns: () => (activeCategory) => {
+            return [
+                    "column1",
+                    "column2",
+                    "column3"
+            ];
+        },
+        getColumnDescription: () => (columnName) => {
+            return "descr_" + columnName;
+        }
+    },
+    mutations: {
+        removeColumn: () => (activeCategory, columnName) => {}
+    }
+};
+
+const props = {
+    activeCategory: "someCategory"
+};
+
+
+describe("columns annotation", () =>  {
+    it("displays columns mapped to the current category and their descriptions", () => {
+        cy.mount(annotColumns, {
+            computed: store.getters,
+            propsData: props
+
+        });
+        const columns = ["column1", "column2", "column3"];
+        columns.forEach(column => {
+            cy.get("[data-cy='mappedColumns']").contains(column);
+            cy.get("[data-cy='mappedColumns']").contains("descr_" + column);
+        });
+
+    });
+    it("displays remove button and informs the store when a column is removed/unlinked at the click of the button", () => {
+        cy.spy(store, 'commit').as('commitSpy');
+        cy.mount(annotColumns, {
+            computed: store.getters,
+            propsData: props,
+            mocks: {
+                $store: store
+            }
+        });
+        cy.get("[data-cy='remove_column2']").click();
+        cy.get("@commitSpy").should("have.been.calledOnceWith", "removeColumn", "someCategory", "column2");
+    });
+});
+
+
