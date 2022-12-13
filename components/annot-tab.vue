6b08230f7d7bcc24321b613505847beff3811389
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-12-12 23:06:18 -0500

    Pass activeCategory prop to explanation component

--- components/annot-tab.vue
+++ components/annot-tab.vue
@@ -3,7 +3,7 @@
     <div :data-cy="activeCategory">
 
         <!-- Explanation text for this annotation tab -->
-        <annot-explanation :data-cy="('annot-expl-' + activeCategory)" />
+        <annot-explanation :data-cy="('annot-expl-' + activeCategory)" :active-category="activeCategory" />
 
         <!-- Lists all the columns linked to the category of this annotation tab -->
         <annot-columns :data-cy="('annot-col-' + activeCategory)" :active-category="activeCategory" />
