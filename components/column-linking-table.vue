a3cd72e9cfa74eb51cfdb8bfbc213411181856c4
rmanaem <rmanaem@protonmail.ch>
2022-12-15 17:29:06 -0500

    Refactored column-linking-table component
    Component reflects the changes following the implementation of alterColumnCategoryMapping mutation

--- components/column-linking-table.vue
+++ components/column-linking-table.vue
@@ -19,8 +19,8 @@
 
 <script>
 
-    // Allows for reference to store actions (index.js)
-    import { mapActions } from "vuex";
+    // Allows for reference to store mutations (index.js)
+    import { mapMutations } from "vuex";
 
     // Allows for reference to store data by creating simple, implicit getters
     import { mapGetters } from "vuex";
@@ -29,7 +29,7 @@
 
         props: {
 
-            selectedCategory: { type: String, required: true }
+            activeCategory: { type: String, required: true }
         },
 
         data() {
@@ -70,21 +70,21 @@
 
         methods: {
 
-            ...mapActions([
+            ...mapMutations([
 
-                "alterColumnCategoryRelation"
+                "alterColumnCategoryMapping"
             ]),
 
             applyCategory(p_row, p_index, p_event) {
 
                 const payload = {
 
-                    category: this.selectedCategory,
+                    category: this.activeCategory,
                     column: p_row.column
                 };
 
-                // 1. Link or unlink the currently-selected category and the clicked column
-                this.alterColumnCategoryRelation(payload);
+                // 1. Link or unlink the currently-selected/active category and the clicked column
+                this.alterColumnCategoryMapping(payload);
 
                 // NOTE: Component emitted column linking info back to
                 // categorization page here for possible next page accessibility
