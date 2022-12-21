3281a668e621576d20d9a8b979cd80f040934c0d
Sebastian Urchs <sebastian.urchs@gmail.com>
2022-12-21 12:06:57 -0500

    columnDescription getter implemented

---
+++ cypress/unit/store-getter-getColumnDescription.cy.js
@@ -0,0 +1,24 @@
+import { getters } from "~/store/index-refactor";
+
+
+const state = {
+    dataDictionary : {
+        annotated: {
+            "describedColumn": {
+                description: "This is my first column"
+            },
+            "lazyColumn": {}
+        }
+    }
+};
+
+describe("getColumnDescription", () => {
+    it("returns description for a column if one exists", () => {
+        const result = getters.getColumnDescription(state, "describedColumn");
+        expect(result).to.be.equal("This is my first column");
+    });
+    it("returns an empty string if no descripton exists", () => {
+        const result = getters.getColumnDescription(state, "lazyColumn");
+        expect(result).to.be.empty;
+    });
+});
