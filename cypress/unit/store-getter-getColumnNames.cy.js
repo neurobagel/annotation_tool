bda242647cecc2a7152b11dd90b0b03151f96c1c
rmanaem <rmanaem@protonmail.ch>
2022-12-22 23:13:02 -0500

    Implemented unit test for getColumnNames getter
    Also added "lint-fix" command to package.json file

---
+++ cypress/unit/store-getter-getColumnNames.cy.js
@@ -0,0 +1,20 @@
+import { getters } from "~/store/index-refactor";
+
+describe("getColumnNames", () => {
+    it("Returns an array of all column names in the data table", () => {
+        const { getColumnNames } = getters;
+    const state = {
+        dataTable: [
+
+            {
+                "column1": "value1", "column2": "value2"
+            },
+            {
+                "column1": "value3", "column2": "value4"
+            }
+        ]
+    };
+    const columnNames = getColumnNames(state);
+    expect(columnNames).to.deep.equal(["column1", "column2"]);
+    });
+});
