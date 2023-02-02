ad505fcee299fdbfd29b179c68d2a2cdaa97d07d
Jonathan Armoza <jarmoza@gmail.com>
2023-02-02 16:45:48 -0500

    getHeuristic unit test

---
+++ cypress/unit/store-getter-getHeuristic.cy.js
@@ -0,0 +1,45 @@
+import { getters } from "~/store";
+
+describe("The column-linking-table component", () => {
+
+    let store = {};
+
+    beforeEach(() => {
+
+        store = {
+
+            state: {
+
+                dataDictionary: {
+
+                    userProvided: {},
+                    annotated: {
+
+                        column1: {}
+                    }
+                }
+            }
+        };
+    });
+
+    it("Get the heuristic of a column with no heuristic", () => {
+
+        // Act
+        const transformationHeuristic = getters.getHeuristic(store.state)("column1");
+
+        // Assert
+        expect(transformationHeuristic).to.equal("");
+    });
+
+    it("Get the heuristic of a column with an already set heuristic", () => {
+
+        // Setup
+        store.state.dataDictionary.annotated.column1.transformationHeuristic = "column1Heuristic";
+
+        // Act
+        const transformationHeuristic = getters.getHeuristic(store.state)("column1");
+
+        // Assert
+        expect(transformationHeuristic).to.equal("column1Heuristic");
+    });
+});
