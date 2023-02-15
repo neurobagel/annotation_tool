aaa7f033a748bf6d1c2171654be38a39321867b3
Jonathan Armoza <jarmoza@gmail.com>
2023-02-15 15:36:53 -0500

    changeMissingStatus unit test

---
+++ cypress/unit/store-mutation-changeMissingStatus.cy.js
@@ -0,0 +1,51 @@
+import { mutations } from "~/store";
+
+let store;
+
+describe("changeMissingStatus mutation", () => {
+
+    // Setup
+    beforeEach(() => {
+
+        store = {
+
+            state: {
+
+                missingValues: {
+
+                    column1: []
+                }
+            }
+        };
+    });
+
+    it("Mark a value as missing", () => {
+
+        // Act
+        mutations.changeMissingStatus(store.state, {
+            column: "column1",
+            markAsMissing: true,
+            value: "value1"
+        });
+
+        // Assert
+        expect(store.state.missingValues.column1).to.include("value1");
+    });
+
+    it("Remove missing status of a value", () => {
+
+        // Setup
+        store.state.missingValues.column1.push("value1");
+
+        // Act
+        mutations.changeMissingStatus(store.state, {
+            column: "column1",
+            markAsMissing: false,
+            value: "value1"
+        });
+
+        // Assert
+        expect(store.state.missingValues.column1).to.not.include("value1");
+    });
+
+});
