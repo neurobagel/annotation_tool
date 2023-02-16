b73b8b69907f2cc66598e71b34636d5b06f2a15d
Jonathan Armoza <jarmoza@gmail.com>
2023-02-16 11:18:54 -0500

    Note changeMissingStatus for future c.values comp

--- cypress/component/annot-continuous-values.cy.js
+++ cypress/component/annot-continuous-values.cy.js
@@ -24,6 +24,8 @@ const store = {
 
     mutations: {
 
+        // NOTE: changeMissingStatus reflects a future 'mark as missing' feature
+        // for the continous value component
         changeMissingStatus: () => (p_columnName, p_rawValue, p_markAsMissing) => {},
         setHeuristic: () => (p_state, { p_column, p_heuristic }) => {}
     }
