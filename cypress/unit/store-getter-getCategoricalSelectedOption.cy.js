3afd41519cab4f2a8ad184508968d4b537b97d92
Jonathan Armoza <jarmoza@gmail.com>
2023-03-15 15:51:42 -0400

    Unit test for getCategoricalSelectedOption

---
+++ cypress/unit/store-getter-getCategoricalSelectedOption.cy.js
@@ -0,0 +1,56 @@
+import { getters } from "~/store";
+
+let store;
+
+describe("getCategoricalSelectedOption", () => {
+
+    beforeEach(() => {
+
+        store = {
+
+            state: {
+
+                dataDictionary: {
+
+                    annotated: {
+
+                        column1: {
+
+                            valueMap: {
+
+                                "raw_value1": "annotated_value1"
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    });
+
+    it("Get the value mapped to a raw value exists in a column that exists in the value map", () => {
+
+        // Act
+        const mappedValue = getters.getCategoricalSelectedOption(store.state)("column1", "raw_value1");
+
+        // Assert
+        expect(mappedValue).to.equal("annotated_value1");
+    });
+
+    it("Attempt to get a value mapped to raw value that does not exist in a column that exists in the value map", () => {
+
+        // Act
+        const mappedValue = getters.getCategoricalSelectedOption(store.state)("column1", "raw_value2");
+
+        // Assert - If a raw value is not found in the column in the value map, a blank string should be returned
+        expect(mappedValue).to.equal("");
+    });
+
+    it("Attempt to get a value mapped to a raw value in a column that does not exist in the value map", () => {
+
+        // Act
+        const mappedValue = getters.getCategoricalSelectedOption(store.state)("column2", "raw_value1");
+
+        // Assert - If a column is not found in the value map, a blank string should be returned
+        expect(mappedValue).to.equal("");
+    });
+});
