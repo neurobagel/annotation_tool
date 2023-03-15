5e9346ed3a2c47cd2848a4d4efca6ffc801902e0
Jonathan Armoza <jarmoza@gmail.com>
2023-03-15 17:49:56 -0400

    Adding/altering tests - alterColumnCategoryMapping

--- cypress/unit/store-mutation-alterColumnCategoryMapping.cy.js
+++ cypress/unit/store-mutation-alterColumnCategoryMapping.cy.js
@@ -1,41 +1,86 @@
 import { mutations } from "~/store";
 
+// Setup
+let store;
+
 describe("alterColumnCategoryMapping", () => {
+
+    beforeEach(() => {
+
+        store = {
+
+            state: {
+
+                columnToCategoryMapping : {
+
+                    "column1": "Age",
+                    "column2": null,
+                    "column3": "Sex",
+                    "column4": null,
+                    "column5": null
+                },
+
+                dataDictionary: {
+
+                    annotated: {
+
+                        column3: {
+
+                        }
+                    },
+
+                    userProvided: {
+
+                    }
+                }
+            }
+        };
+    });
 
     it("Removes the mapping of column to category if they're already mapped", () => {
 
-        const { alterColumnCategoryMapping } = mutations;
-        const state = {
-            columnToCategoryMapping : {
+        // Act
+        mutations.alterColumnCategoryMapping(store.state, { category: "Sex", column: "column3" });
 
-                "column1": "Age",
-                "column2": null,
-                "column3": "Sex",
-                "column4": null,
-                "column5": null
-          }
-      };
-
-      alterColumnCategoryMapping(state, { category: "Sex", column: "column3" });
-      expect(state.columnToCategoryMapping.column3).to.equal(null);
+        // Assert
+        expect(store.state.columnToCategoryMapping.column3).to.equal(null);
     });
 
     it("Changes the mapping of column to category if they're not already mapped", () => {
 
-        const { alterColumnCategoryMapping } = mutations;
-        const state = {
-            columnToCategoryMapping : {
-                "column1": "Age",
-                "column2": null,
-                "column3": "Sex",
-                "column4": null,
-                "column5": null
-            }
-        };
+        // Act
+        mutations.alterColumnCategoryMapping(store.state, { category: "someCategory", column: "column1" });
 
-        alterColumnCategoryMapping(state, { category: "someCategory", column: "column1" });
-        expect(state.columnToCategoryMapping.column1).to.equal("someCategory");
-        alterColumnCategoryMapping(state, { category: "someCategory", column: "column2" });
-        expect(state.columnToCategoryMapping.column2).to.equal("someCategory");
+        // Assert
+        expect(store.state.columnToCategoryMapping.column1).to.equal("someCategory");
+        expect(store.state.dataDictionary.annotated.column1).to.deep.equal({ description: "" });
+
+        // Act
+        mutations.alterColumnCategoryMapping(store.state, { category: "someCategory", column: "column2" });
+
+        // Assert
+        expect(store.state.columnToCategoryMapping.column2).to.equal("someCategory");
+        expect(store.state.dataDictionary.annotated.column2).to.deep.equal({ description: "" });
+    });
+
+    it("Unlink a category from a column that is not listed in the user provided data dictionary", () => {
+
+        // Act
+        mutations.alterColumnCategoryMapping(store.state, { category: "Sex", column: "column3" });
+
+        // Assert
+        expect(store.state.dataDictionary.annotated["column3"]).to.deep.equal({ "description": "" });
+    });
+
+    it("Unlink a category from a column that is listed in the user provided data dictionary", () => {
+
+        // Setup
+        store.state.dataDictionary.userProvided["column3"] = { description: "filled in description" };
+
+        // Act
+        mutations.alterColumnCategoryMapping(store.state, { category: "Sex", column: "column3" });
+
+        // Assert
+        expect(store.state.dataDictionary.annotated["column3"]).to.deep.equal({ description: "filled in description" });
     });
 });
