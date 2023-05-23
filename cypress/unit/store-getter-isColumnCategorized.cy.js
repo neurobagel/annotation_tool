4da3a004214a621a6b8d2e25593001f5e96089ba
Jonathan Armoza <jarmoza@gmail.com>
2023-05-23 14:09:38 -0400

    Unit test for isColumnCategorized getter

---
+++ cypress/unit/store-getter-isColumnCategorized.cy.js
@@ -0,0 +1,23 @@
+import { getters } from "~/store";
+
+let store = {
+
+    getters: getters,
+
+    state: {
+
+        columnToCategoryMap: { column1: "category1" }
+    }
+};
+
+describe("isColumnCategorized", () => {
+
+    it("Check for column categorization via columnToCategoryMap", () => {
+
+        // Assert - Returns true if column is a key in 'columnToCategoryMap'
+        expect(store.getters.isColumnCategorized(store.state)("column1")).to.be.true;
+
+        // Assert - Returns false if column is not a key in 'columnToCategoryMap'
+        expect(store.getters.isColumnCategorized(store.state)("column2")).to.be.false;
+    });
+});
