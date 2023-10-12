b7050b4aa12113f493275bce09936ad16a9f0019
Arman Jahanpour <77515879+rmanaem@users.noreply.github.com>
2023-10-12 15:28:53 -0400

    [MNT] Implemented `getColumnsForCategory` and `getUniqueColumnValues` (#569)
    * Implemented `getColumnsForCategory` and `getUniqueColumnValues`
    
    Refactored `getUniqueValues` and moved some of its logic to the
    newly implemented getters
    
    * Refactored `getUniqueValues` unit test

--- cypress/unit/store-getter-getUniqueValues.cy.js
+++ cypress/unit/store-getter-getUniqueValues.cy.js
@@ -1,47 +1,57 @@
 import { getters } from "~/store";
 
 let store;
+let state;
 
 describe("getUniqueValues getter", () => {
 
     beforeEach(() => {
+        state = {
+
+            columnToCategoryMap: {
+                column1: "Diagnosis",
+                column2: "Age",
+                column3: "Sex"
+            },
+
+            dataDictionary: {
+
+                annotated: {
+
+                    column1: { missingValues: [] },
+                    column2: { missingValues: [] },
+                    column3: { missingValues: ["column3_value1", "column3_value2"] }
+                }
+            },
+
+            dataTable: [
+
+                { column1: 1, column2: "column2_value1", column3: "column3_value1" },
+                { column1: 2, column2: "column2_value2", column3: "column3_value2" },
+                { column1: 1, column2: "column2_value3", column3: "column3_value3" },
+                { column1: 3, column2: "column2_value2", column3: "column3_value4" }
+            ]
+        };
 
         store = {
+            getters: {
+                // getColumnsForCategory: () => (p_category) => {
+                //     return ["column1", "column2", "column3"];
+                // },
+                getColumnsForCategory: getters.getColumnsForCategory(state),
 
-            state: {
+                getUniqueColumnValues: getters.getUniqueColumnValues(state)
+            },
 
-                columnToCategoryMap: {
-
-                    column1: "Diagnosis",
-                    column2: "Age",
-                    column3: "Sex"
-                },
-
-                dataDictionary: {
-
-                    annotated: {
-
-                        column1: { missingValues: [] },
-                        column2: { missingValues: [] },
-                        column3: { missingValues: ["column3_value1", "column3_value2"] }
-                    }
-                },
-
-                dataTable: [
-
-                    { column1: 1, column2: "column2_value1", column3: "column3_value1" },
-                    { column1: 2, column2: "column2_value2", column3: "column3_value2" },
-                    { column1: 1, column2: "column2_value3", column3: "column3_value3" },
-                    { column1: 3, column2: "column2_value2", column3: "column3_value4" }
-                ]
-            }
-        };
-    });
+            state: state
+            };
+        }
+    );
 
     it("Retrieves unique values of a single column", () => {
 
         // Act
-        const diagnosisUniqueValues = getters.getUniqueValues(store.state)("Diagnosis");
+        const diagnosisUniqueValues = getters.getUniqueValues(store.state, store.getters)("Diagnosis");
 
         // Assert
         expect(diagnosisUniqueValues).to.deep.equal({ "column1": [1, 2, 3] });
@@ -50,10 +60,11 @@ describe("getUniqueValues getter", () => {
     it("Retrieves unique values of a multiple columns", () => {
 
         // Setup
+        // We change the state by reference because the state passed to the getters needs to reflect that change
         store.state.columnToCategoryMap.column2 = "Diagnosis";
 
         // Act
-        const diagnosisUniqueValues = getters.getUniqueValues(store.state)("Diagnosis");
+        const diagnosisUniqueValues = getters.getUniqueValues(store.state, store.getters)("Diagnosis");
 
         // Assert
         expect(diagnosisUniqueValues).to.deep.equal({
@@ -66,7 +77,7 @@ describe("getUniqueValues getter", () => {
     it("Retrieve unique value list of maximum length", () => {
 
         // Act
-        const diagnosisUniqueValues = getters.getUniqueValues(store.state)("Diagnosis", 2);
+        const diagnosisUniqueValues = getters.getUniqueValues(store.state, store.getters)("Diagnosis", 2);
 
         // Assert
         expect(diagnosisUniqueValues).to.deep.equal({ "column1": [1, 2]});
@@ -75,7 +86,7 @@ describe("getUniqueValues getter", () => {
     it("Make sure no missing values are included in unique values", () => {
 
         // Act
-        const diagnosisUniqueValues = getters.getUniqueValues(store.state)("Sex");
+        const diagnosisUniqueValues = getters.getUniqueValues(store.state, store.getters)("Sex");
 
         // Assert
         expect(diagnosisUniqueValues).to.deep.equal({ "column3": ["column3_value3", "column3_value4"]});
