49a62969d58962e83317a15be7ce4ccdfad499a9
rmanaem <rmanaem@protonmail.ch>
2023-11-02 11:49:20 -0400

    Implemented validate-output test

---
+++ cypress/e2e/validate-output.cy.js
@@ -0,0 +1,37 @@
+const Ajv = require('ajv');
+
+const outputJSONFilePath = 'cypress/fixtures/examples/good/ds003653_participant_annotated_1698934398962.json';
+
+const exampleSyntheticFilePath = 'cypress/fixtures/examples/good/example_synthetic.json';
+
+const exampleOutputSyntheticFilePath = 'cypress/fixtures/examples/good/example_synthetic copy_annotated_1698938921939.json';
+
+const schemaPath = 'cypress/fixtures/schema/neurobagel_data_dictionary.schema.json';
+
+
+describe('Validate Output', () => {
+  it('Validates example output against the schema', () => {
+
+    const ajv = new Ajv();
+
+    cy.readFile(schemaPath)
+          .then((schema) => {
+
+            const validate = ajv.compile(schema);
+
+            cy.readFile(outputJSONFilePath)
+              .then((outputJSONData) => {
+
+                const isValid = validate(outputJSONData);
+
+                if (isValid) {
+                  cy.log('JSON is valid according to the schema');
+                } else {
+                  cy.log('JSON is not valid according to the schema');
+                  cy.log(validate.errors);
+                }
+                cy.expect(isValid).to.be.true;
+      });
+    });
+  });
+});
